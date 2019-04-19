2c1713ffbecaab6186ad069b8457e35dfeb0b9de
Faaiz Joad <fjoad@andrew.cmu.edu>
2019-04-19 22:40:09 +0000

    basic login page added

---
+++ app/helpers/demos_helper.rb
@@ -0,0 +1,29 @@
+module DemosHelper
+    def login(user)
+        session[:user_id] = user.id
+        #session[:role] = params[:demo][:role]
+    end
+    
+    def current_user
+        if @current_user.nil?
+            @current_user = User.find_by(id: session[:user_id])
+         #   if session[:role] == 'professor'
+          #      @current_user = Professor.find_by(id: session[:user_id])
+           # else
+            #    @current_user = Student.find_by(id: session[:user_id])
+            #end
+        else
+            @current_user
+        end
+    end
+    
+    def logged_in?
+        !current_user.nil?
+    end
+    
+    def logout
+        session.delete(:user_id)
+        #session.delete(:role)
+        @current_user = nil
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   resources :jobs
   resources :shifts
   resources :flavors
+  resources :demos, only: [:new, :create, :destroy]
+  get 'demos/new', to: 'demos#new', as: :login
+  get 'demos/destroy', to: 'demos#destroy', as: :logout
   get 'page/mainpage'
     resources :stores
     resources :employees
     resources :assignments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'page#mainpage'
+  root 'demos#new'
 end
